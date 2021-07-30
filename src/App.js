8bd6b808c1eb2d32733aad96b478196696c65759
MykalB <mykalb081@gmail.com>
2021-07-30 19:42:54 -0400

    Update App.js
    - Refactor code
    - Added search bar
    - Made some styling changes

--- src/App.js
+++ src/App.js
@@ -1,73 +1,98 @@
 import React from 'react';
+
 import Container from 'react-bootstrap/Container';
 import Button from 'react-bootstrap/Button'
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
 import Carousel from 'react-bootstrap/Carousel';
 import Card from 'react-bootstrap/Card';
-import CardGroup from 'react-bootstrap/CardGroup';
+import Form from 'react-bootstrap/Form'
+import FormControl from 'react-bootstrap/FormControl'
 
 import './App.css';
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      cryptos: [
+        {
+          name: "Bitcoin",
+          imageUrl: "https://upload.wikimedia.org/wikipedia/commons/thumb/4/46/Bitcoin.svg/480px-Bitcoin.svg.png",
+          infoUrl: "https://bitcoin.org" 
+        },
+        {
+          name: "Ethereum",
+          imageUrl: "https://upload.wikimedia.org/wikipedia/commons/6/6f/Ethereum-icon-purple.svg",
+          infoUrl: "https://ethereum.org"
+        },
+        {
+          name: "Dogecoin",
+          imageUrl: "https://upload.wikimedia.org/wikipedia/en/d/d0/Dogecoin_Logo.png",
+          infoUrl: "https://dogecoin.com/"
+        }
+      ]
+    };
+  }
   render() {
     return (
       <Container fluid>
-        <Navbar bg="dark" variant="dark" expand="lg">
-          <Container>
-            <Navbar.Brand href="#home">Crypto-Coins</Navbar.Brand>
-            <Nav>
-              <Nav.Link href="#features">Features</Nav.Link>
-              <Nav.Link href="#pricing">Pricing</Nav.Link>
-            </Nav>
-          </Container>
-        </Navbar>
-
-        <Carousel className="mt-2">
-        <Carousel.Item>
-          <Card bg="dark" text="light" className="text-center py-5">
-            <Card.Img variant="top" src="https://img.icons8.com/ios/100/000000/bitcoin.png" style={{ width: '100px', display: 'block', margin: 'auto' }}/>
-            <Card.Body>
-              <Card.Title>Bitcoin</Card.Title>
-              <Card.Text>Price value in USD</Card.Text>
-              <Button variant="primary">Go somewhere</Button>
-            </Card.Body>
-            <Card.Footer>
-              <small>Last updated: 5 mins ago</small>
-            </Card.Footer>
-          </Card>
-          </Carousel.Item>
-          <Carousel.Item>
-          <Card bg="dark" text="light" className="text-center py-5">
-            <Card.Img variant="top" src="https://img.icons8.com/ios/100/000000/ethereum.png" style={{ width: '100px', display: 'block', margin: 'auto' }}/>
-            <Card.Body>
-              <Card.Title>Ethereum</Card.Title>
-              <Card.Text>Price value in USD</Card.Text>
-              <Button variant="primary">Go somewhere</Button>
-            </Card.Body>
-            <Card.Footer>
-              <small>Last updated: 5 mins ago</small>
-            </Card.Footer>
-          </Card>
-          </Carousel.Item>
-          <Carousel.Item>
-          <Card bg="dark" text="light" className="text-center py-5">
-            <Card.Img variant="top" src="https://img.icons8.com/ios/100/000000/dogecoin.png" style={{ width: '100px', display: 'block', margin: 'auto' }}/>
-            <Card.Body>
-              <Card.Title>Dogecoin</Card.Title>
-              <Card.Text>Price value in USD</Card.Text>
-              <Button variant="primary">Go somewhere</Button>
-            </Card.Body>
-            <Card.Footer>
-              <small>Last updated: 5 mins ago</small>
-            </Card.Footer>
-          </Card>
-          </Carousel.Item>
-        </Carousel>
-
+        <QuickView cryptos = { this.state.cryptos } />
+        <Navigation />
+        <div>
+          hello
+        </div>
       </Container>
     );
   }
+}
+
+let Navigation = (props) => {
+  return (
+    <Navbar bg="secondary" expand="lg">
+      <Container>
+        <Navbar.Brand href="#home">Crypto-Coins</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav>
+            <Nav.Link href="#features">Features</Nav.Link>
+            <Nav.Link href="#pricing">Pricing</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+        <Form className="d-flex">
+          <FormControl
+            type="search"
+            placeholder="Search"
+            className="mr-2"
+            aria-label="Search"
+          />
+          <Button variant="outline-dark">Search</Button>
+        </Form>
+      </Container>
+    </Navbar>
+  );
+}
+
+let QuickView = (props) => {
+  const quickViewItems = props.cryptos.map( (crypto, index) => {
+    return (
+      <Carousel.Item key = { index }>
+        <Card bg="secondary" className="text-center py-5">
+          <Card.Img variant="top" src={ crypto.imageUrl } style={{ width: '100px', display: 'block', margin: 'auto' }}/>
+          <Card.Body>
+            <Card.Title as="h1">{ crypto.name }</Card.Title>
+            <Card.Text>Price value in USD</Card.Text>
+            <small>Last updated: 5 mins ago</small>
+          </Card.Body>
+        </Card>
+      </Carousel.Item>
+    );
+  });
+  return (
+    <Carousel className="mt-2">
+      {quickViewItems}
+    </Carousel>
+  );
 }
 
 export default App;
